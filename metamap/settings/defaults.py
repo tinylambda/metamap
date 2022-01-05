e262900cdf1deec1471422e7d7b375b77b5c4a11
Felix <tinylambda@gmail.com>
2022-01-05 10:09:27 +0800

    Add comments on gunicorn configurations

--- metamap/settings/defaults.py
+++ metamap/settings/defaults.py
@@ -200,42 +200,123 @@ ASGI_SERVICE_PROCESSES = max(OS_CPU_COUNT - 1, 1)
 # This is the content of Gunicorn conf file, a valid Python file
 # https://docs.gunicorn.org/en/stable/settings.html#settings
 GUNICORN_CONFIG = f'''
+# The socket to bind. can bind to multiple addresses
 bind = ['127.0.0.1:8000', ]
+
+# The maximum number of pending connections.
 backlog = 2048
+
+# The number of worker processes for handling requests.
 workers = {WSGI_SERVICE_PROCESSES}
+
+# The type of workers to use.
 worker_class = 'sync'
+
+# The number of worker threads for handling requests.
+# If you try to use the sync worker type and set the threads setting to more than 1, 
+# the gthread worker type will be used instead.
 threads = {WSGI_SERVICE_THREADS_PER_PROCESS}
+
+# The maximum number of simultaneous clients.
+# This setting only affects the Eventlet and Gevent worker types.
 worker_connections = 1000
+
+# The maximum number of requests a worker will process before restarting.
 max_requests = 0
+
+# The maximum jitter to add to the max_requests setting. to avoid all workers restarting at the same time.
 max_requests_jitter = 0
+
+# Workers silent for more than this many seconds are killed and restarted.
 timeout = 30
+
+# After receiving a restart signal, workers have this much time to finish serving requests
 graceful_timeout = 30
+
+# The number of seconds to wait for requests on a Keep-Alive connection. 
+# When Gunicorn is deployed behind a load balancer, it often makes sense to set this to a higher value.
+# sync worker does not support persistent connections and will ignore this option.
 keepalive = 60
+
+# A WSGI application path in pattern $(MODULE_NAME):$(VARIABLE_NAME).
 wsgi_app = '{WSGI_APPLICATION_REPR}'
+
+# Restart workers when code changes. This setting is intended for development
 reload = False
+
+# The implementation that should be used to power reload.
 reload_engine = 'auto'
+
+# Extends reload option to also watch and reload on additional files
 reload_extra_files = []
+
+# Install a trace function that spews every line executed by the server.
+# This is the nuclear option.
 spew = False
+
+# Check the configuration and exit. 
+# The exit status is 0 if the configuration is correct, and 1 if the configuration is incorrect.
 check_config = False
+
+# Print the configuration settings as fully resolved. Implies check_config.
 print_config = False
+
+# The Access log file to write to. '-' means log to stdout.
 accesslog = '-'
+
+# Disable redirect access logs to syslog.
 disable_redirect_access_to_syslog = False
+
+# The access log format. (https://docs.gunicorn.org/en/stable/settings.html#access-log-format)
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# The Error log file to write to.
+# Using '-' for FILE makes gunicorn log to stderr.
 errorlog = '-'
+
+# The granularity of Error log outputs.
 loglevel = 'info'
+
+# Redirect stdout/stderr to specified file in errorlog.
 capture_output = False
+
+# The logger you want to use to log events in Gunicorn.
 logger_class = 'gunicorn.glogging.Logger'
+
+# The log config file to use. Gunicorn uses the standard Python logging module’s Configuration file format.
 logconfig = None
+
+# The log config dictionary to use, using the standard Python logging module’s dictionary configuration format.
 logconfig_dict = {{}}
+
+# Address to send syslog messages. (https://docs.gunicorn.org/en/stable/settings.html#syslog-addr)
 syslog_addr = 'udp://localhost:514'
+
+# Send Gunicorn logs to syslog.
 syslog = False
+
+# Makes Gunicorn use the parameter as program-name in the syslog entries.
 syslog_prefix = None
+
+# Syslog facility name
 syslog_facility = 'user'
+
+# Enable stdio inheritance.
 enable_stdio_inheritance = False
+
+# host:port of the statsd server to log to.
 statsd_host = None
+
+# A comma-delimited list of datadog statsd (dogstatsd) tags to append to statsd metrics.
 dogstatsd_tags = ''
+
+# Prefix to use when emitting statsd metrics (a trailing . is added, if not provided).
 statsd_prefix = ''
+
+# A base to use with setproctitle for process naming.
 proc_name = '{MAIN_MODULE_NAME}'
+
+# Internal setting that is adjusted for each type of application.
 default_proc_name = 'gunicorn'
 
 # The maximum size of HTTP request line in bytes.
@@ -247,43 +328,83 @@ limit_request_fields = 100
 # Limit the allowed size of an HTTP request header field.
 limit_request_field_size = 8190
 
+# Load application code before the worker processes are forked.
+# By preloading an application you can save some RAM resources as well as speed up server boot times. 
+# Although, if you defer application loading to each worker process, 
+# you can reload your application code easily by restarting workers.
 preload_app = True
 
+# Disables the use of sendfile().
+# If not set, the value of the SENDFILE environment variable is used to enable or disable its usage.
 sendfile = None
 
+# Set the SO_REUSEPORT flag on the listening socket.
 reuse_port = True
 
+# Change directory to specified directory before loading apps.
 chdir = '{str(BASE_DIR)}'
 
+# Daemonize the Gunicorn process.
 daemon = False
 
+# Set environment variables in the execution environment.
+# Should be a list of strings in the key=value format.
 raw_env = []
 
+# A filename to use for the PID file.
+# If not set, no PID file will be written.
 pidfile = None
 
+# A directory to use for the worker heartbeat temporary file.
+# If not set, the default temporary directory will be used.
 worker_tmp_dir = None
 
+# Switch worker processes to run as this user.
 # user = 'felix'
+
+# Switch worker process to run as this group.
 # group = 'felix'
+
+# A bit mask for the file mode on files written by Gunicorn.
 # umask = 0022
+
+# If true, set the worker process’s group access list with all of the groups 
+# of which the specified username is a member, plus the specified group id.
 # initgroups = False
 
+# Directory to store temporary request data as they are read.
+# If not specified, Gunicorn will choose a system generated temporary directory.
+# This may disappear in the near future.
 tmp_upload_dir = None
 
+# A dictionary containing headers and values that the front-end proxy uses to indicate HTTPS requests.
 secure_scheme_headers = {{
     'X-FORWARDED-PROTOCOL': 'ssl', 
     'X-FORWARDED-PROTO': 'https', 
     'X-FORWARDED-SSL': 'on',
 }}
 
+# Front-end’s IPs from which allowed to handle set secure headers. (comma separate).
+# Set to * to disable checking of Front-end IPs
 forwarded_allow_ips = '*'
 
+# A comma-separated list of directories to add to the Python path.
 pythonpath = None
+
+# Load a PasteDeploy config file. 
 paste = None
+
+# Enable detect PROXY protocol
 proxy_protocol = False
 
+# Front-end’s IPs from which allowed accept proxy requests (comma separate).
+# Set to * to disable checking of Front-end IPs 
 proxy_allow_ips = '*'
+
+# Set a PasteDeploy global config variable in key=value form.
 raw_paste_global_conf = []
+
+# Strip spaces present between the header name and the the :.
 strip_header_spaces = False
 '''
 GUNICORN_CONFIG_FILENAME = os.path.join(BASE_DIR, 'run', f'gunicorn_{MAIN_MODULE_NAME}.py')
