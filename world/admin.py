95ad11dacb0e45f557afa041739590837b4c6349
Felix Pan <tinylambda@gmail.com>
2022-02-19 22:58:22 +0800

    core framework of defining concepts and their relations

--- world/admin.py
+++ world/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from world.models import Attrib, Concept
+
+
+class AttribInline(admin.TabularInline):
+    model = Attrib
+    extra = 0
+    fields = ('name', 'datatype', 'metadata')
+    ordering = ('time_added', )
+    show_full_result_count = True
+    show_change_link = True
+
+
+class ConceptAdmin(admin.ModelAdmin):
+    inlines = [AttribInline, ]
+    list_display = ('name', 'hint', 'time_updated', 'time_added')
+    ordering = ('-time_added', )
+
+
+admin.site.register(Concept, ConceptAdmin)
