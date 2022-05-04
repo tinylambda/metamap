b811477b6155195d1fd886af8fcbdb6e28cd11d9
Felix Pan <tinylambda@gmail.com>
2022-05-04 10:06:06 +0800

    A more class-style view

--- server/views.py
+++ server/views.py
@@ -1,7 +1,14 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
 
+@method_decorator(csrf_exempt, name="dispatch")
 class ServerView(View):
+    def __init__(self, **kwargs):
+        super(ServerView, self).__init__(**kwargs)
+        self.response_dict = {}
+
     def get(self, request):
-        return HttpResponse('Hello servers')
+        return JsonResponse(self.response_dict)
