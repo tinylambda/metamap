4ade67a06b3bb6bcfc63922fb14e51335d186c16
Felix Pan <tinylambda@gmail.com>
2022-05-21 01:40:41 +0800

    use pytest for testing

--- server/models.py
+++ server/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class GoodTable(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    content = models.TextField(default='')
+
+
+class BadTable(models.Model):
+    good_table = models.ForeignKey(
+        GoodTable,
+        on_delete=models.CASCADE,
+        related_name='bad_tables',
+        db_constraint=False,
+    )
+    change_time = models.DateTimeField(auto_now_add=True)
--- server/tests.py
+++ server/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+import pytest
+from django.db import IntegrityError
 
-# Create your tests here.
+from .models import GoodTable, BadTable
+
+
+@pytest.fixture
+def sample_good():
+    good = GoodTable(name='name_sample', content='content_sample')
+    good.save()
+    return good
+
+
+def test_model_save(transactional_db):
+    record = GoodTable(name='name1', content='content1')
+    with pytest.raises(ValueError) as e:
+        record.save(force_update=True)
+    assert e.value.args[0] == 'Cannot force an update in save() with no primary key.'
+
+    record.save(force_insert=True)
+
+    with pytest.raises(IntegrityError) as e:
+        record.save(force_insert=True)
+    assert e.value.args[0] == 'UNIQUE constraint failed: server_goodtable.id'
+
+
+def test_foreign_key(transactional_db, sample_good):
+    bad = BadTable()
+    with pytest.raises(IntegrityError) as e:
+        bad.save()
+    assert (
+        e.value.args[0] == ' NOT NULL constraint failed: server_badtable.good_table_id'
+    )
