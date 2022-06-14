afbdff6e08a1ec2db65c3b61ab98194279380dd6
Felix Pan <tinylambda@gmail.com>
2022-06-14 23:19:59 +0800

    add airflow

---
+++ core/dj_airflow.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+from airflow.operators.python import PythonOperator
+
+DJANGO_ENVIRONMENT_HAS_BEEN_SETUP = False
+
+
+def setup_django_for_airflow(path_to_settings_py: str):
+    global DJANGO_ENVIRONMENT_HAS_BEEN_SETUP
+
+    if DJANGO_ENVIRONMENT_HAS_BEEN_SETUP:
+        return
+
+    base_directory = os.path.dirname(path_to_settings_py)
+    project_name = os.path.basename(path_to_settings_py)
+
+    # Add Django project root to path
+    sys.path.append(base_directory)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{project_name}.settings")
+
+    import django
+
+    django.setup()
+
+    DJANGO_ENVIRONMENT_HAS_BEEN_SETUP = True
+
+
+class DjangoOperator(PythonOperator):
+    path_to_settings_py: str = os.getenv(
+        "AIRFLOW_DJANGO_PATH_TO_SETTINGS_PY", "/usr/src/app/project"
+    )
+
+    def pre_execute(self, *args, **kwargs):
+        setup_django_for_airflow(self.path_to_settings_py)
